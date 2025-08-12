0aebe189804de0b3e54758f7b54979cc7252dda7
Oke <qocepes@gmail.com>
2025-08-12 22:37:40 +0100

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import "./index.css";
-import FarmingInnovationSection from "./components/FarmingInnovationSection";
-import Footer from './components/Footer';
-import HeroSection from "./components/HeroSection";
-import AboutSection from "./components/AboutSection";
-import ContactForm from "./components/ContactForm";
 import Prompt from "./components/prompt";
 import OurServices from "./components/OurServices";
 
@@ -18,12 +13,7 @@ const HomePage: React.FC = () => {
   return (
     <>
       <Navbar onJoinClick={handleJoinClick} />
-      <HeroSection />
-      <FarmingInnovationSection />
-      <AboutSection />
       <OurServices />
-      <ContactForm />
-      <Footer />
     </>
   );
 };
