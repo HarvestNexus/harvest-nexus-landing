6036c1352a01b9805fbde1a6fab481985f964bab
TORY-SKY <141348591+TORY-SKY@users.noreply.github.com>
2025-08-18 20:39:18 +0100

    Update FarmingInnovationSection.tsx

--- src/components/FarmingInnovationSection.tsx
+++ src/components/FarmingInnovationSection.tsx
@@ -75,7 +75,7 @@ const FarmingInnovationSection: React.FC = () => {
         <div className="space-y-8">
           <div>
             <h2 className="font-['Noto_Sans'] text-3xl md:text-4xl font-semibold text-black">
-              Farmers Earn More. Buyers Get fresher. Everyone Wins
+              Farmers Earn More. Buyers Get Fresher. Everyone Wins
             </h2>
             <p className="mt-4 text-gray-600">{/*sub texts area*/}</p>
           </div>
