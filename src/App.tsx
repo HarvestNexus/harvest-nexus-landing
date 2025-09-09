4fd534dc55e9ba0ffa8c1e49f2351cb5d4bc25e1
Oke <qocepes@gmail.com>
2025-09-09 19:26:59 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
-import { Navbar } from "./components/Navbar/Navbar";
-import "./index.css";
-import FarmingInnovationSection from "./components/FarmingInnovationSection";
-import Footer from './components/Footer';
-import HeroSection from "./components/HeroSection";
-import AboutSection from "./components/AboutSection";
-import ContactForm from "./components/ContactForm";
-import Prompt from "./components/prompt";
-import OurServices from "./components/OurServices";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
-
-const HomePage: React.FC = () => {
-  const navigate = useNavigate();
-  const handleJoinClick = () => {
-    navigate("/Prompt");
-  };
-  return (
-    <>
-      <Navbar onJoinClick={handleJoinClick} />
-      <HeroSection />
-      <FarmingInnovationSection />
-      <AboutSection />
-      <OurServices />
-      <ContactForm />
-      <Footer />
-    </>
-  );
-};
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="min-h-screen">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Prompt" element={<><Navbar onJoinClick={() => {}} /><Prompt /></>} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* placeholders for now */}
+        <Route path="/dashboard" element={<h1 className="p-6">Dashboard Page</h1>} />
+        <Route path="/login" element={<h1 className="p-6">Login Page</h1>} />
+        <Route path="/signup" element={<h1 className="p-6">Signup Page</h1>} />
+      </Routes>
     </Router>
   );
 };
+
 export default App;
