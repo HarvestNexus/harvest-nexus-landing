8773d27fbc8a64d6b25a7b514801ebf9a71bf346
Oke <qocepes@gmail.com>
2025-09-09 19:42:22 +0100

    l'mkljmn

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import "./index.css";
+import FarmingInnovationSection from "./components/FarmingInnovationSection";
 import Footer from './components/Footer';
 import HeroSection from "./components/HeroSection";
-
+import AboutSection from "./components/AboutSection";
+import ContactForm from "./components/ContactForm";
 import Prompt from "./components/prompt";
+import OurServices from "./components/OurServices";
 
 
 const HomePage: React.FC = () => {
@@ -17,6 +20,10 @@ const HomePage: React.FC = () => {
     <>
       <Navbar onJoinClick={handleJoinClick} />
       <HeroSection />
+      <FarmingInnovationSection />
+      <AboutSection />
+      <OurServices />
+      <ContactForm />
       <Footer />
     </>
   );
@@ -24,7 +31,7 @@ const HomePage: React.FC = () => {
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="min-h-screen border-b-8 border-l-8 border-r-8 border-green-400 sm:px-8 lg:px-10">
+      <div className="min-h-screen">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/Prompt" element={<><Navbar onJoinClick={() => {}} /><Prompt /></>} />
